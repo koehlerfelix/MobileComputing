0002b62bdc17b473bd28b4a346b1d029c634d60d
koehlerfelix <33344306+koehlerfelix@users.noreply.github.com>
2019-03-11 21:52:19 +0100

    Add files via upload

--- Webseite/js/function.js
+++ Webseite/js/function.js
@@ -22,3 +22,35 @@ function openMenu() {
         }, 250);
     }
 }
+
+
+// Distanzmessung ------------------------
+var valueP;
+function getLocation() {
+    valueP = document.getElementById("valueP");
+     if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showDistance, showError);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showDistance(position) {
+    valueP.innerHTML = " ";
+    var actualLatitude = position.coords.latitude;
+    var actualLongitude = position.coords.longitude;
+    
+    var jerryLatitude = 49.2948033;
+    var jerryLongitude = 8.73504156;
+    
+    var dLatitude = Math.abs(jerryLatitude - actualLatitude);
+    var dLongitude = Math.abs(jerryLongitude - actualLongitude);
+    //calculate distance 1degree = 111,12km
+     var distance = (Math.sqrt(dLatitude * dLatitude + dLongitude * dLongitude) * 111.120).toFixed(3);
+    
+    setTimeout(function() {valueP.innerHTML = distance + "  Kilometer";},
+        100);	
+}
+function showError(error) {
+    valueP.innerHTML = "Es gab ein Problem. Möglicherweiße keine Internetverbindung.";
+}
